f772941e163eabab7c1504a38c620e09e31aa905
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2017-02-11 19:18:36 -0600

    Actualizando...

---
+++ apuntes-y-recursos-didacticos/p02_torsion/toBeamer.bat
@@ -0,0 +1,5 @@
+@ECHO OFF 
+pdflatex -interaction=nonstopmode pres.tex
+pdflatex -interaction=nonstopmode pres.tex
+pres.pdf
+exit
---
+++ tareas/problemario_u1/toPDF.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+pdflatex -interaction=nonstopmode problemario_01.tex
+pdflatex -interaction=nonstopmode problemario_01.tex
+problemario_01.pdf
+exit
